f988a63ebed4b6327823f568a33453550c5ba3ba
joshua-hale <joshuahale173@gmail.com>
2025-10-05 16:23:45 +0100

    Create validSudoku.java

---
+++ validSudoku.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char current = board[r][c];
+                if (current == ".") continue;
+
+                int digitIndex = current - 1;
+
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r][digitIndex] || cols[c][digitIndex] || boxes[boxIndex][digitIndex]) {
+                    return false;
+                }
+
+                rows[r][digitIndex] = true;
+                cols[c][digitIndex] = true;
+                boxes[boxIndex][digitIndex] = true;
+            }
+        }
+        return true;
+    }
