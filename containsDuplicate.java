f733e96783087df6c44ae25988143b17a56201e1
joshua-hale <joshuahale173@gmail.com>
2025-10-06 16:55:15 +0100

    Update containsDuplicate.java

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,19 +1,9 @@
-import java.util.HashMap;
-
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i]; 
-
-            if (map.containsKey(complement)) {
-                return new int[] {map.get(complement), i};
-            }
-
-            map.put(nums[i], i);
+public boolean containsDuplicate(int[] nums) {
+    Set<Integer> seen = new HashSet<>();
+    for (int num : nums) {
+        if (!seen.add(num)) {  // add() returns false if num already exists
+            return true;
         }
-
-        throw new IllegalArgumentException("No solution");
     }
+    return false;
 }
