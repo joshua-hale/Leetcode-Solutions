0b9f8843112aabbc91ff008ffaf52003cf05ea2e
joshua-hale <joshuahale173@gmail.com>
2025-10-06 17:26:31 +0100

    Update missingNumber.java

--- missingNumber.java
+++ missingNumber.java
@@ -15,3 +15,21 @@ class Solution {
         throw new IllegalArgumentException("No solution");
     }
 }
+
+// O(1) Space and O(n) time solution
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        // Expected sum of numbers from 0 to n
+        int expectedSum = n * (n + 1) / 2;
+
+        // Actual sum of the array
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        // The missing number is the difference
+        return expectedSum - actualSum;
+    }
+}
