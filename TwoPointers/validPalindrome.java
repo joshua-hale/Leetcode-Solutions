bc985f4e7b9ac1186b17657580685594eab300cf
joshua-hale <joshuahale173@gmail.com>
2025-10-07 22:10:33 +0100

    Create validPalindrome.java

---
+++ TwoPointers/validPalindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+
+        StringBuilder cleaned = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if(Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
