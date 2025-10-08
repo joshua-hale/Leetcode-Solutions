496667fc88a254e005041e632c41923ce2d13354
joshua-hale <joshuahale173@gmail.com>
2025-10-08 18:25:03 +0100

    Create containerWithMostWater.java

---
+++ TwoPointers/containerWithMostWater.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int length = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = length * h;
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
