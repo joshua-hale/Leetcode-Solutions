3ea4a763384dfe88445f6c954e45ff784540608a
joshua-hale <joshuahale173@gmail.com>
2025-10-10 18:57:06 +0100

    Update longestConsecutiveSequence.java

--- ArraysHashing/longestConsecutiveSequence.java
+++ ArraysHashing/longestConsecutiveSequence.java
@@ -2,31 +2,28 @@ import java.util.*;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
 
         Set<Integer> set = new HashSet<>();
+        int best = 0;
+
         for (int num : nums) {
             set.add(num);
         }
 
-        int best = 0;
-
         for (int num : set) {
             if (!set.contains(num - 1)) {
                 int current = num;
-                int streak = 1;
+                int localBest = 0;
 
-                while (set.contains(current + 1)) {
-                    current++;
-                    streak++;
+                while (set.contains(current)) {
+                    localBest += 1;
+                    current += 1;
                 }
 
-                
-                best = Math.max(best, streak);
+                best = Math.max(best, localBest);
             }
         }
+
         return best;
     }
 }
